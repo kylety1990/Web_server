8b5db13565099cfa57c067a94a0c558353f57266
Christian <cristian001_13@hotmail.com>
2019-08-20 22:50:12 +0200

    ouhh Mama

--- server.js
+++ server.js
@@ -1,42 +1,33 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+
 const hbs = require('hbs');
+require('./hbs/helpers');
 
 const port = process.env.PORT || 3000;
-require('./hbs/helpers')
 
-app.use(express.static(__dirname + '/public'))
 
-hbs.registerPartials(__dirname + '/views/partials');
+app.use(express.static(__dirname + '/public'));
 
-//Express HBS
+// Express HBS engine
+hbs.registerPartials(__dirname + '/views/parciales');
 app.set('view engine', 'hbs');
 
+
+
+
 app.get('/', (req, res) => {
-    // res.send('Hello World')
 
     res.render('home', {
-        nombre: "Maquina",
-        anio: new Date().getFullYear(),
+        nombre: 'fernando'
     });
-
-})
+});
 
 app.get('/about', (req, res) => {
 
-
-    res.render('about', {
-        anio: new Date().getFullYear(),
-    });
-
-})
-
-// app.get('/Home', (req, res) => {
-//     let home = "Soy el rey del mundoo!!";
-
-//     res.send(home)
-// })
+    res.render('about');
+});
 
 app.listen(port, () => {
-    console.log(`Escuchando en el puerto ${port}`);
-})
+    console.log(`Escuchando peticiones en el puerto ${ port }`);
+});
